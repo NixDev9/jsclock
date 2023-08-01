c5ed60f1101c35921ab3cfbfdf8df1fcb254b379
Nix <134012290+NixDev9@users.noreply.github.com>
2023-07-31 21:40:28 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,50 +1,30 @@
 function updateClock() {
   const clockElement = document.getElementById('clock');
   const dateElement = document.getElementById('date');
-  const timezoneElement = document.getElementById('timezone');
 
-  // Get the visitor's timezone abbreviation using Intl.DateTimeFormat
-  const timezoneAbbreviation = new Intl.DateTimeFormat('en-US', { timeZoneName: 'short' })
-    .format(new Date())
-    .split(' ')[2];
-
-  // Get the current time in the visitor's timezone
-  const now = new Date();
-  const timezoneOffsetMinutes = now.getTimezoneOffset();
+  // Get the visitor's timezone offset in minutes
+  const timezoneOffsetMinutes = new Date().getTimezoneOffset();
 
   // Create a new date object with the timezone offset applied
-  const localTime = new Date(now.getTime() + (timezoneOffsetMinutes * 60 * 1000));
-
-  // Determine whether Daylight Saving Time (DST) is in effect
-  const isDST = (() => {
-    const january = new Date(localTime.getFullYear(), 0, 1);
-    const july = new Date(localTime.getFullYear(), 6, 1);
-    const stdTimezoneOffset = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset());
-    return localTime.getTimezoneOffset() < stdTimezoneOffset;
-  })();
-
-  // Add the DST offset (1 hour) if DST is in effect
-  const adjustedTimezoneOffset = timezoneOffsetMinutes + (isDST ? 60 : 0);
-  const adjustedLocalTime = new Date(now.getTime() + (adjustedTimezoneOffset * 60 * 1000));
+  const localTime = new Date(Date.now() + (timezoneOffsetMinutes * 60 * 1000));
 
   // Format the date as Month DD, YYYY
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const month = months[adjustedLocalTime.getMonth()];
-  const day = adjustedLocalTime.getDate();
-  const year = adjustedLocalTime.getFullYear();
+  const month = months[localTime.getMonth()];
+  const day = localTime.getDate();
+  const year = localTime.getFullYear();
   const dateString = `${month} ${day}, ${year}`;
 
   // Format the time as hh:mm:ss AM/PM (12-hour format)
-  const hours = adjustedLocalTime.getHours() % 12 || 12;
-  const minutes = adjustedLocalTime.getMinutes().toString().padStart(2, '0');
-  const seconds = adjustedLocalTime.getSeconds().toString().padStart(2, '0');
-  const meridiem = adjustedLocalTime.getHours() >= 12 ? 'PM' : 'AM';
+  const hours = localTime.getHours() % 12 || 12;
+  const minutes = localTime.getMinutes().toString().padStart(2, '0');
+  const seconds = localTime.getSeconds().toString().padStart(2, '0');
+  const meridiem = localTime.getHours() >= 12 ? 'PM' : 'AM';
   const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
 
-  // Update the clock, date, and timezone elements with the visitor's local time and date
+  // Update the clock and date elements with the visitor's local time and date
   clockElement.textContent = timeString;
   dateElement.textContent = dateString;
-  timezoneElement.textContent = timezoneAbbreviation;
 }
 
 // Call the updateClock function every second
@@ -52,4 +32,3 @@ setInterval(updateClock, 1000);
 
 // Run the updateClock function immediately to display the initial time
 updateClock();
-
