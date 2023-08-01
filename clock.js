e54320de507e4b3d6c6ba8cf0d148d8fcb2b2d0a
Nix <134012290+NixDev9@users.noreply.github.com>
2023-07-31 20:58:39 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,11 +1,20 @@
-// Function to update the clock every second
 function updateClock() {
   const dateElement = document.getElementById('date');
   const timeElement = document.getElementById('time');
 
   // Get the current time in the visitor's timezone
   const now = new Date();
-  const timezoneOffsetMinutes = now.getTimezoneOffset();
+
+  // Determine whether Daylight Saving Time (DST) is in effect
+  const isDST = (() => {
+    const january = new Date(now.getFullYear(), 0, 1);
+    const july = new Date(now.getFullYear(), 6, 1);
+    const stdTimezoneOffset = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset());
+    return now.getTimezoneOffset() < stdTimezoneOffset;
+  })();
+
+  // Add the DST offset (1 hour) if DST is in effect
+  const timezoneOffsetMinutes = now.getTimezoneOffset() + (isDST ? 60 : 0);
   const localTime = new Date(now.getTime() - (timezoneOffsetMinutes * 60 * 1000));
 
   // Get the timezone abbreviation using Intl.DateTimeFormat
@@ -13,11 +22,12 @@ function updateClock() {
     .format(localTime)
     .split(' ')[2];
 
-  // Format the time as HH:mm:ss
-  const hours = localTime.getHours().toString().padStart(2, '0');
+  // Format the time as hh:mm:ss AM/PM
+  const hours = localTime.getHours() % 12 || 12;
   const minutes = localTime.getMinutes().toString().padStart(2, '0');
   const seconds = localTime.getSeconds().toString().padStart(2, '0');
-  const timeString = `${hours}:${minutes}:${seconds} ${timezoneAbbreviation}`;
+  const meridiem = localTime.getHours() >= 12 ? 'PM' : 'AM';
+  const timeString = `${hours}:${minutes}:${seconds} ${meridiem} ${timezoneAbbreviation}`;
 
   // Format the date as YYYY-MM-DD
   const year = localTime.getFullYear();
