d842e30c0d76c578b1fcf3f3c93c9aaa39f34469
Nix <134012290+NixDev9@users.noreply.github.com>
2023-08-05 18:30:16 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,42 +1,28 @@
-function updateClock() {
-  const clockElement = document.getElementById('clock');
-  const dateElement = document.getElementById('date');
-  const timezoneElement = document.getElementById('timezone');
-
-  // Get the visitor's timezone offset in minutes
-  const timezoneOffsetMinutes = new Date().getTimezoneOffset();
-
-  // Create a new date object with the timezone offset applied
-  const localTime = new Date(Date.now() + (timezoneOffsetMinutes * 60 * 1000));
-
-  // Format the date as Month DD, YYYY
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const month = months[localTime.getMonth()];
-  const day = localTime.getDate();
-  const year = localTime.getFullYear();
-  const dateString = `${month} ${day}, ${year}`;
-
-  // Format the time as hh:mm:ss AM/PM (12-hour format)
-  const hours = localTime.getHours() % 12 || 12;
-  const minutes = localTime.getMinutes().toString().padStart(2, '0');
-  const seconds = localTime.getSeconds().toString().padStart(2, '0');
-  const meridiem = localTime.getHours() >= 12 ? 'PM' : 'AM';
-  const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
-
-  // Get the visitor's timezone abbreviation using Intl.DateTimeFormat
-  const timezoneAbbreviation = new Intl.DateTimeFormat('en-US', { timeZoneName: 'short' })
-    .format(localTime)
-    .split(' ')[2];
-
-  // Update the clock, date, and timezone elements with the visitor's local time and date
-  clockElement.textContent = timeString;
-  dateElement.textContent = dateString;
-  timezoneElement.textContent = timezoneAbbreviation;
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Handle midnight (0 o'clock)
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
 }
 
-// Run the updateClock function immediately to display the initial time
-updateClock();
+function updateClock() {
+  const now = new Date();
+  const timeString = formatAMPM(now);
+  const dateString = now.toLocaleDateString();
 
-// Call the updateClock function every second
+  document.getElementById('clock').innerHTML = `
+    <div>${timeString}</div>
+    <div>Text in the middle</div>
+    <div>${dateString}</div>
+  `;
+}
+
+// Call the updateClock function every second to keep the clock updated
 setInterval(updateClock, 1000);
 
+// Initial call to display the clock immediately
+updateClock();
